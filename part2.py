b59b5c630af833232b595e9bfc9d9f4dc7e8605e
Ademir Vieira <vieiraademir707@gmail.com>
2024-08-08 12:18:27 -0300

    parte 2 test

--- part2.py
+++ part2.py
@@ -1,6 +1,13 @@
 import random
 
-abelhas_pequenas = 200
+num =str(input("coloque o numero da população: "))
+# numero da população de especies
+abelhas_pequenas = num
 
 for abelhas in range(abelhas_pequenas):
-    abelhas_eliminadas = random.randint(0,4)
+    # abelhas de 1 a 4 podem morrer
+    chance_de_morte = random.randint(0,4)
+    if chance_de_morte == 4:
+        abelhas_pequenas -= -1
+
+print(str(abelhas_pequenas) + "abelhas pequenas que sobreviveram")
